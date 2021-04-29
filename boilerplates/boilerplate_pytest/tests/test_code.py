477f98f5cb01935815ecb7a0a36265c27877e23f
Paul Fry <paul.fry@versent.com.au>
2021-04-29 14:04:16 +1000

    housekeeping

--- boilerplates/boilerplate_pytest/tests/test_code.py
+++ boilerplates/boilerplate_pytest/tests/test_code.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python Version  : 3.7
+* Name          : test_code.py
+* Description   : Boilerplate pytest python script
+* Created       : 26-02-2021
+* Usage         : pytest tests/test_code.py
+"""
+
+__author__ = "Paul Fry"
+__version__ = "0.1"
+
 from src.boilerplate import is_even
 
 def test_valid_even():
