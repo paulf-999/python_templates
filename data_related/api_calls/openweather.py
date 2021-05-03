408326a1f4e33b78dd737266774d384b2e665e17
Paul Fry <paul.fry@versent.com.au>
2021-05-03 18:21:10 +1000

    added api call scripts

---
+++ data_related/api_calls/openweather.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Python Version  : 3.7
+* Name          : openweather.py
+* Description   : Script to perform an API call to gain open weather data
+* Created       : 26-02-2021
+* Usage         : python3 openweather.py
+
+see: https://knasmueller.net/using-the-open-weather-map-api-with-python
+https://home.openweathermap.org/api_keys
+"""
+
+import os
+import sys
+from datetime import datetime
+from time import time
+import logging
+import requests
+import json
+
+#import custom modules
+
+working_dir = os.getcwd()
+# Set up a specific logger with our desired output level
+logging.basicConfig(format='%(message)s')
+logger = logging.getLogger('application_logger')
+logger.setLevel(logging.INFO)
+# by default, turn off log outputs. But if desired, change this arg to True
+#logger.propagate = False
+
+def get_weather_data(lat, lon, api_key):
+    """ Main entry point of the app """
+    START_TIME = time()
+    logger.debug(f"Function called: get_weather_data()")
+
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+
+    response = requests.get(url)
+    weather_data = json.loads(response.text)
+    #logger.debug(weather_data)
+
+    timezone = weather_data["timezone"]
+    timezone_offset = weather_data["timezone_offset"]
+    current = weather_data["current"]
+    hourly = weather_data["hourly"]
+    daily = weather_data["daily"]
+
+    logger.debug(hourly)
+    logger.debug(f"Function finished: get_weather_data() finished in {round(time() - START_TIME, 2)} seconds")
+
+    return
+
+if __name__ == '__main__':
+    """ This is executed when run from the command line """
+    lat = "-37.811291"
+    lon = "144.985916"
+    api_key = ""
+
+    get_weather_data(lat, lon, api_key)
