7af090ceb65649dc71317f9dab79370523bfa68c
Paul Fry <paul.fry@versent.com.au>
2021-07-14 10:39:26 +1000

    added template for jinja, and removed redundant f-strings in boilerplate.py script

---
+++ other/jinja/jinja.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Python Version  : 3.7
+* Name          : jinja.py
+* Description   : Demo jinja template script
+* Created       : 14-07-2021
+* Usage         : python3 jinja.py
+"""
+
+__author__ = "Paul Fry"
+__version__ = "0.1"
+
+import os
+from jinja2 import Environment, FileSystemLoader
+
+working_dir = os.getcwd()
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+
+    template_dir = os.path.join(working_dir, "templates")
+    jinja_env = Environment(loader=FileSystemLoader(template_dir), autoescape=True)
+    template = jinja_env.get_template("eg_template.sql.j2")
+
+    data_src = "eg_src"
+    src_db = "eg_src_db"
+    src_db_schema = "eg_src_db_schema"
+    src_tbl_name = "eg_src_tbl_name"
+
+    rendered_op = template.render(data_src=data_src, src_db=src_db, src_db_schema=src_db_schema, src_tbl_name=src_tbl_name)
+
+    with open(os.path.join(working_dir, "op", "eg_op.sql"), "w") as op:
+        op.write(rendered_op)
---
+++ other/jinja/op/eg_op.sql
@@ -0,0 +1,9 @@
+version: 2
+
+sources:
+  - name: eg_src
+    database: eg_src_db
+    schema: eg_src_db_schema
+    tables:
+      - name: eg_src_tbl_name
+      - name: eg_src_tbl_name
