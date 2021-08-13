bca960b59b0ff47dac678829627b0124b8ff70ec
Paul Fry <paul-fry@hotmail.com>
2021-08-13 14:38:02 +1000

    resolved linting issues

--- data_related/api_calls/openweather.py
+++ data_related/api_calls/openweather.py
@@ -18,26 +18,27 @@ import logging
 import requests
 import json
 
-#import custom modules
+# import custom modules
 
 working_dir = os.getcwd()
 # Set up a specific logger with our desired output level
-logging.basicConfig(format='%(message)s')
-logger = logging.getLogger('application_logger')
+logging.basicConfig(format="%(message)s")
+logger = logging.getLogger("application_logger")
 logger.setLevel(logging.INFO)
 # by default, turn off log outputs. But if desired, change this arg to True
-#logger.propagate = False
+# logger.propagate = False
+
 
 def get_weather_data(lat, lon, api_key):
-    """ Main entry point of the app """
+    """Main entry point of the app"""
     START_TIME = time()
-    logger.debug(f"Function called: get_weather_data()")
+    logger.debug("Function called: get_weather_data()")
 
     url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
 
     response = requests.get(url)
     weather_data = json.loads(response.text)
-    #logger.debug(weather_data)
+    # logger.debug(weather_data)
 
     timezone = weather_data["timezone"]
     timezone_offset = weather_data["timezone_offset"]
@@ -45,13 +46,19 @@ def get_weather_data(lat, lon, api_key):
     hourly = weather_data["hourly"]
     daily = weather_data["daily"]
 
-    logger.debug(hourly)
+    logger.debug(f"timezone = {timezone}")
+    logger.debug(f"timezone_offset = {timezone_offset}")
+    logger.debug(f"current = {current}")
+    logger.debug(f"hourly = {hourly}")
+    logger.debug(f"daily = {daily}")
+
     logger.debug(f"Function finished: get_weather_data() finished in {round(time() - START_TIME, 2)} seconds")
 
     return
 
-if __name__ == '__main__':
-    """ This is executed when run from the command line """
+
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
     lat = "-37.811291"
     lon = "144.985916"
     api_key = ""
