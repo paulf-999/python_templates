d41e982c31509f0fca1941e0ceb446d46fa3b833
Paul Fry <paul-fry@hotmail.com>
2022-10-28 20:38:13 +1100

    revised sf conn script

--- 1_templates/3_data_related_templates/db_related/validate_sf_conn.py
+++ 1_templates/3_data_related_templates/db_related/validate_sf_conn.py
@@ -1,14 +1,64 @@
 #!/usr/bin/env python
+import logging
+import os
+
 import snowflake.connector
+import yaml
 
-# Gets the version
-ctx = snowflake.connector.connect(user="", password="", account="")
-cs = ctx.cursor()
 
-try:
-    cs.execute("SELECT current_version()")
-    one_row = cs.fetchone()
-    print(one_row[0])
-finally:
-    cs.close()
-ctx.close()
+def get_logger():
+    """Set up a specific logger with our desired output level"""
+    logging.basicConfig(format="%(message)s")
+    logger = logging.getLogger("application_logger")
+    logger.setLevel(logging.INFO)
+
+    return logger
+
+
+def read_ip():
+    """Read input from config file"""
+
+    working_dir = os.getcwd()
+
+    # with open(os.path.join(working_dir, "ip", "config_mine.yaml")) as ip_yml:
+    with open(os.path.join(working_dir, "ip", "config.yaml")) as ip_yml:
+        data = yaml.safe_load(ip_yml)
+
+    snowflake_username = data["db_connection_params"]["snowflake_username"]
+    snowflake_pass = data["db_connection_params"]["snowflake_pass"]
+    snowflake_account = data["db_connection_params"]["snowflake_account"]
+
+    return snowflake_username, snowflake_pass, snowflake_account
+
+
+def main():
+    """Main entry point of the app"""
+    logger = get_logger()
+
+    # get inputs
+    snowflake_username, snowflake_pass, snowflake_account = read_ip()
+
+    conn = snowflake.connector.connect(
+        user=snowflake_username,
+        password=snowflake_pass,
+        account=snowflake_account
+    )
+
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT current_version()")
+        one_row = cursor.fetchone()
+        logger.info(f"\nSnowflake version = {one_row[0]}\n")
+    finally:
+        cursor.close()
+
+    conn.close()
+
+    return
+
+
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
+
+    main()
