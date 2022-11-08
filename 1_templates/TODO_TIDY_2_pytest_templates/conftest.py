07629537b00fab7fd908ff0ce3249f5764cdd5ee
Paul Fry <paul-fry@hotmail.com>
2022-11-08 22:19:46 +1100

    sweep of housekeeping changes

--- 1_templates/TODO_TIDY_2_pytest_templates/conftest.py
+++
@@ -1,2 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--pytest_example_arg", action="store")
--- 1_templates/TODO_TIDY_2_pytest_templates/tests/conftest.py
+++
@@ -1,2 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--example_arg", action="store")
